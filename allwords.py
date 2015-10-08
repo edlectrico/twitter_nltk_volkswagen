e62eea4a69faec877a95c0df164c47ca470acce5
edlectrico <edlectrico@gmail.com>
2015-10-08 09:37:40 +0200

    Processing in Spanish

--- allwords.py
+++ allwords.py
@@ -3,31 +3,36 @@ import os
 import sys
 from nltk.corpus import wordnet
 
-input_csv = pd.read_csv('output/vw_clean_en_rechecked.csv')
-input_df = pd.DataFrame(data=input_csv)
+def generate_all_words(language):
+  input_lang = language[0].lower()
 
-input_classified_csv = pd.read_csv('output/vw_classified.csv')
-input_classified_df = pd.DataFrame(data=input_classified_csv)
+  input_csv = pd.read_csv('output/vw_clean_' + input_lang + '_rechecked.csv')
+  input_df = pd.DataFrame(data=input_csv)
 
-output_df = pd.DataFrame(columns=['id', 'word', 'classification'])
+  input_classified_csv = pd.read_csv('output/vw_classified_'+ input_lang + '.csv')
+  input_classified_df = pd.DataFrame(data=input_classified_csv)
 
-for index, row in input_df.iterrows():
-  try:
-    row_words = str(row['text'])
-    words = row_words.split(' ')
-    for word in words:
-      if wordnet.synsets(word):
-        classification_row = input_classified_df.loc[input_classified_df['id'] == row['id']]
-        classification = classification_row['classification']
-        # print(str(classification))
-        # break
-        final_classification = 'pos'
-        if 'neg' in str(classification):
-          final_classification = 'neg'
-        output_df = output_df.append({'id':row['id'], 'word':word, 'classification':final_classification}, ignore_index=True)
-  except KeyboardInterrupt:
-    print('Exiting...')
-    output_df.to_csv('output/allwords_classified_by_tweet.csv')
-    sys.exit()
+  output_df = pd.DataFrame(columns=['id', 'word', 'classification'])
 
-output_df.to_csv('output/allwords_classified_by_tweet.csv')
+  for index, row in input_df.iterrows():
+    try:
+      row_words = str(row['text'])
+      words = row_words.split(' ')
+      for word in words:
+        if wordnet.synsets(word):
+          classification_row = input_classified_df.loc[input_classified_df['id'] == row['id']]
+          classification = classification_row['classification']
+          final_classification = 'pos'
+          if 'neg' in str(classification):
+            final_classification = 'neg'
+          output_df = output_df.append({'id':row['id'], 'word':word, 'classification':final_classification}, ignore_index=True)
+    except KeyboardInterrupt:
+      print('Writing to...output/allwords_classified_by_tweet_' + input_lang + '.csv')
+      output_df.to_csv('output/allwords_classified_by_tweet_' + input_lang + '.csv')
+      sys.exit()
+
+  print('Writing to...output/allwords_classified_by_tweet_' + input_lang + '.csv')
+  output_df.to_csv('output/allwords_classified_by_tweet_' + input_lang + '.csv')
+
+if __name__ == '__main__':
+  generate_all_words(sys.argv[1:])
--- vw_sentiment_classification.py
+++ vw_sentiment_classification.py
@@ -4,23 +4,28 @@ import tweet_preprocessor as prep
 import pandas as pd
 import sys
 
-vw_original = pd.read_csv('output/vw_clean_en_rechecked.csv')
-vw = pd.DataFrame(data=vw_original)
+def classify_tweets(language):
+  input_lang = language[0].lower()
 
-vw_out = pd.DataFrame(columns=['id', 'classification'])
+  vw_original = pd.read_csv('output/vw_clean_' + input_lang + '_rechecked.csv')
+  vw = pd.DataFrame(data=vw_original)
 
-for index, row in vw.iterrows():
-  try:
-    if len(str(row['text']).split()) > 1:
-      print (s.sentiment(str(row['text'])))
-      vw_out = vw_out.append({'id':str(row['id']),
-			'classification':str(s.sentiment(str(row['text']))[0])},
-			ignore_index = True)
+  vw_out = pd.DataFrame(columns=['id', 'classification'])
 
-  except KeyboardInterrupt:
-    print('Writing to file...')
-    vw_out.to_csv('output/vw_classified.csv')
-    sys.exit()
+  for index, row in vw.iterrows():
+    try:
+      if len(str(row['text']).split()) > 1:
+        print (s.sentiment(str(row['text'])))
+        vw_out = vw_out.append({'id':str(row['id']),
+  			'classification':str(s.sentiment(str(row['text']))[0])},
+  			ignore_index = True)
+    except KeyboardInterrupt:
+      print('Writing to file...output/vw_classified_' + input_lang + '.csv')
+      vw_out.to_csv('output/vw_classified_' + input_lang + '.csv')
+      sys.exit()
 
-print('Writing to file...')
-vw_out.to_csv('output/vw_classified.csv')
+  print('Writing to file...output/vw_classified_' + input_lang + '.csv')
+  vw_out.to_csv('output/vw_classified_' + input_lang + '.csv')
+
+if __name__ == '__main__':
+  classify_tweets(sys.argv[1:])
